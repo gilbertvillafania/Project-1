40480cc02a872658336ad3404a4c6025c388733d
shrill <cheryl.olonan@gmail.com>
2019-04-11 20:41:17 -0700

    more playing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
 
-<<<<<<< HEAD
-  var superhero = $(this).attr("data-name");
-  var queryURL = "https://comicvine.gamespot.com/api"  + superhero + "&api_key=b057b892091e49452a4d3b1ea5baa460104470f0&";
-=======
   $(document).on("click", "#add-superhero", displaySuperhero);
 
 });
@@ -14,19 +10,26 @@ function displaySuperhero(event) {
 
   var superhero = $(this).attr("name");
   var queryURL = "https://comicvine.gamespot.com/api"  + superhero + "b057b892091e49452a4d3b1ea5baa460104470f0";
->>>>>>> 06cce63b76533c50ed4624fe931e3f59db2fb10e
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-
-    superheroDiv = $("<div class='superhero'>")
-    var image = response.image;
-    console.log(response.image);
-    var displayimage = $("<img>").attr("src", image);
-    superheroDiv.append(displayimage);
-
+  $.ajaxPrefilter(function (options) {
+    if (options.crossDomain && $.support.cors) {
+      options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
   });
 
-}
+$.ajax(url, { headers: { Authorization: 'Bearer 7RrX7J.........' }})
+    .then(function(response) {
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
+
+        superheroDiv = $("<div class='superhero'>")
+        var image = response.image;
+        console.log(response.image);
+        var displayimage = $("<img>").attr("src", image);
+        superheroDiv.append(displayimage);
+
+      });
+
+    )
