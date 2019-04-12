0eb14210609ecde42120acd68dc3688f4b0f3482
hayleeb13 <hayleeb13@gmail.com>
2019-04-11 19:42:00 -0700

    More javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 function displaySuperhero() {
 
-  var superhero = $(this).attr("data-name");
+  var superhero = $(this).attr("name");
   var queryURL = "https://comicvine.gamespot.com/api"  + superhero + "b057b892091e49452a4d3b1ea5baa460104470f0";
 
   $.ajax({
@@ -8,11 +8,13 @@ function displaySuperhero() {
     method: "GET"
   }).then(function(response) {
 
-    var superheroDiv = $("div id 'superhero-image");
+    superheroDiv = $("<div class='superhero'>")
     var image = response.image;
     var displayimage = $("<img>").attr("src", image);
     superheroDiv.append(displayimage);
 
   });
+
+  $(document).on("click", "#add-superhero", displaySuperhero);
 
 }
