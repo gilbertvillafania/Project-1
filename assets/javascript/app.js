376cd8e3f05905cfa5faf6b765ac09e54004667a
shrill <cheryl.olonan@gmail.com>
2019-04-11 20:02:34 -0700

    playing with javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 function displaySuperhero() {
 
   var superhero = $(this).attr("data-name");
-  var queryURL = "https://comicvine.gamespot.com/api"  + superhero + "b057b892091e49452a4d3b1ea5baa460104470f0";
+  var queryURL = "https://comicvine.gamespot.com/api"  + superhero + "&api_key=b057b892091e49452a4d3b1ea5baa460104470f0&";
 
   $.ajax({
     url: queryURL,
