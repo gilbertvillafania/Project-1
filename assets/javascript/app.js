21fefd6bf8f66607cf1578e38b63defd43e85335
hayleeb13 <hayleeb13@gmail.com>
2019-04-13 01:57:31 -0700

    Displays superhero info

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  $("#superheroimage").hide();
   $(document).on("click", "#add-superhero", displaySuperhero);
 
 });
@@ -12,8 +13,6 @@ function displaySuperhero(event) {
 
   var queryURL = "https://superheroapi.com/api/10157355780833919/search/" + superhero;
 
-  console.log(superhero);
-
   $.ajaxPrefilter(function(options) {
     if (options.crossDomain && $.support.cors) {
         options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
@@ -23,13 +22,40 @@ function displaySuperhero(event) {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
 
-    superheroDiv = $("<div class='superhero'");
-    var image = response.image;
-    console.log(response.image);
-    var displayimage = $("<img>").attr("src", image);
-    superheroDiv.append(displayimage);
+/*    console.log(response);
+    if(response.results.length > 1){
+      for (let i = 0; i < response.results.length; i++) {
+        var btn = document.createElement("button");
+        var t = document.createTextNode([i]);
+        btn.appendChild(t);
+        document.body.appendChild(btn);
+      }
+
+    }
+    else {
+    var name = response.results[0].name;
+    var birthplace = response.results[0].biography["place-of-birth"];
+    var mainimage = response.results[0].image.url;
+    }
+*/
+    console.log(response);
+    var name = response.results[0].name;
+    console.log(name);
+    var displayName = $("<p>").text("Name: " + name);
+    $("#superhero-date-of-birth").append(displayName);
+    var birthplace = response.results[0].biography["place-of-birth"];
+    console.log(birthplace);
+    var displaybirthplace = $("<p>").text("Place of Birth: " + birthplace);
+    $("#superhero-date-of-birth").append(displaybirthplace);
+    var powerstats = response.results[0].powerstats.combat;
+    console.log(powerstats);
+    var displayPowerstats = $("<p>").text("Powerstats: " + powerstats);
+    $("#superhero-powers-first-appearance-story-arcs").append(displayPowerstats);
+    var firstappearance = response.results[0].biography["first-appearance"];
+    console.log(firstappearance);
+    var displayfirstappearance = $("<p>").text("First Appearance: " + firstappearance);
+    $("#superhero-powers-first-appearance-story-arcs").append(displayfirstappearance);
 
   });
 
